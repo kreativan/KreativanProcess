526168321f75a8708f17de6967152b915c5c101c
Ivan Milincic <kreativan@outlook.com>
2018-11-01 17:24:44 +0100

    js css files update

---
+++ subpage.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ *  Module Admin UI
+ *
+ *  @author Ivan Milincic <kreativan@outlook.com>
+ *  @copyright 2018 Kreativan
+ *
+ *  @var this_module
+ *
+*/
+
+// $this->session->set("status", "message");
+// $this->session->set("alert", "This is custom admin message");
+
+
+?>
+
+<p>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+</p>
